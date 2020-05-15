c00bdba7df899c2347bc312f413e9a96f5a1c816
szilard-molnar <mszilard86@gmail.com>
2020-05-15 10:57:06 -0400

    label change

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -22,7 +22,7 @@ class SearchBar extends React.Component {
         return(
             <Paper elevation={6} style={{padding: '25px'}}>
                 <form onSubmit={this.handleSubmit}>
-                    <TextField fullWidth label="Search..." onChange={this.handleChange}/>
+                    <TextField fullWidth label="Search video..." onChange={this.handleChange}/>
                 </form>
             </Paper>
         )
