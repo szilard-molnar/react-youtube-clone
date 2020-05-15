0c8a834c862eaf6395d00a10580469a2c5cb0ca3
szilard-molnar <mszilard86@gmail.com>
2020-05-15 19:21:17 -0400

    Updates

---
+++ precache-manifest.352bf8c2a3ad7d63e5a40c01c061fa93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35c575e0f82134a201db43caae36d3d6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "67bb8c7a3a0bfe0d6040",
+    "url": "/static/js/2.c2b0c594.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.c2b0c594.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1bdd24adf09b4bae91a",
+    "url": "/static/js/main.edd38f11.chunk.js"
+  },
+  {
+    "revision": "1390afa2d2dd07935f9e",
+    "url": "/static/js/runtime-main.9b69e10c.js"
+  }
+]);
