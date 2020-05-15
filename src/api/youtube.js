57f9b4574bfa805572c8e6ed509fa90c6a4795f2
szilard-molnar <mszilard86@gmail.com>
2020-05-15 10:32:25 -0400

    .env file added for storing private api key

--- src/api/youtube.js
+++ src/api/youtube.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
+const API_KEY = process.env.REACT_APP_YOUTUBE_API_KEY;
 
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params: {
         part: 'snippet',
         maxResults: 5,
-        key: 'AIzaSyB3IQu57kKna7JPHCivzBM0iOHbkaUHOVY'
+        key: API_KEY
     }
 });
--- src/components/VideoList.js
+++ src/components/VideoList.js
@@ -2,9 +2,13 @@ import React from 'react';
 import { Grid } from '@material-ui/core';
 import VideoItem from './VideoItem';
 
-const VideoList = ({videos}) => {
-    const listOfVideos = videos.map((video) => <VideoItem />)
-    return listOfVideos;
+const VideoList = ({videos, onVideoSelect}) => {
+    const listOfVideos = videos.map((video, id) => <VideoItem key={id} onVideoSelect={onVideoSelect} video={video} />)
+    return (
+        <Grid container spacing={10}>
+            {listOfVideos}
+        </Grid>
+    );
 }
 
 export default VideoList;
