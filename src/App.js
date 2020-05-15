c3f7e80b5ed09a3755f3e4ae8704d7253c47f131
szilard-molnar <mszilard86@gmail.com>
2020-05-15 09:51:39 -0400

    youtube video displays on screen with title, channel, descripition

--- src/App.js
+++ src/App.js
@@ -1,18 +1,39 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import youtube from './api/youtube';
+import { SearchBar, VideoDetail } from './components';
 
 class App extends React.Component {
+    state = {
+        videos: [],
+        selectedVideo: null,
+    }
+
+    handleSubmit = async (searchTerm) => {
+        const response = await youtube.get('search', {
+        params: {
+            part: 'snippet',
+            maxResults: 5,
+            key: 'AIzaSyB3IQu57kKna7JPHCivzBM0iOHbkaUHOVY',
+            q: searchTerm,
+        }
+        });
+
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });    
+    }
+
+
     render(){
+        const { selectedVideo } = this.state;
         return(
-            <Grid justify="center" container spacing={16}>
+            <Grid justify="center" container spacing={10}>
                 <Grid item xs = {12}>
-                    <Grid container spacing={16}>
+                    <Grid container spacing={10}>
                         <Grid item xs = {12}>
-                            {/* SearchBar */}
+                            <SearchBar onFormSubmit={this.handleSubmit}/>
                         </Grid>
                         <Grid item xs = {8}>
-                            {/* Video details */}
+                            <VideoDetail video={selectedVideo}/>
                         </Grid>
                         <Grid item xs = {4}>
                             {/* Video list */}
